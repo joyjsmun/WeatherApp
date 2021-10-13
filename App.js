519f089f6fde8220942355f701e585e9db2c51cb
Joy M <joy.js.mun@gmail.com>
2021-10-13 22:45:06 +0900

    ADD: test layout system

--- App.js
+++ App.js
@@ -4,18 +4,13 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Get some sleeep!!!!!!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, flexDirection: "row" }}>
+      <View style={{ flex: 1, backgroundColor: "#ffff8f" }} />
+      <View style={{ flex: 3, backgroundColor: "#ec9e69" }} />
+      <View style={{ flex: 3, backgroundColor: "#d56073" }} />
+      <View style={{ flex: 1, backgroundColor: "#7a4579" }} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
