91dc696d3c6eeaad73caa484ecdd1fd1cf54c01a
Joy M <joy.js.mun@gmail.com>
2021-10-14 10:01:21 +0900

    ADD: First stylessheet

--- App.js
+++ App.js
@@ -1,16 +1,49 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, flexDirection: "row" }}>
-      <View style={{ flex: 1, backgroundColor: "#ffff8f" }} />
-      <View style={{ flex: 3, backgroundColor: "#ec9e69" }} />
-      <View style={{ flex: 3, backgroundColor: "#d56073" }} />
-      <View style={{ flex: 1, backgroundColor: "#7a4579" }} />
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffff8f",
+  },
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: 30,
+  },
+  cityName: {
+    color: "black",
+    fontSize: 59,
+    fontWeight: "600",
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 40,
+    fontSize: 160,
+  },
+  desc: { fontSize: 40, marginTop: -20 },
+});
